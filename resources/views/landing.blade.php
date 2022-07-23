a07c37a294184e30d832783cedbaa42f053d9d98
fernandy21 <62871388+fernandy21@users.noreply.github.com>
2022-07-23 20:05:34 +0800

    Carousel
    up

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -134,7 +134,7 @@
       <!-- Main Container -->
       <main id="main-container">
 
-        <div class="bg-image" style="background-image: url({{asset('media/photos/photo15@2x.jpg')}});">
+        <!-- <div class="bg-image" style="background-image: url({{asset('media/photos/photo15@2x.jpg')}});">
           <div class="bg-primary-dark-op">
             <div class="content content-full">
               <div class="py-3 text-center">
@@ -143,7 +143,7 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
 
         <!-- Navigation -->
         <div class="bg-sidebar-dark">
@@ -309,7 +309,37 @@
           </div>
         </div>
         <!-- END Navigation -->
+
         <!-- Page Content -->
+        <!-- Carousel -->
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="{{asset('media/photos/1ok.jpg')}}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="{{asset('media/photos/2ok.jpg')}}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="{{asset('media/photos/3ok.jpg')}}" class="d-block w-100" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+        <!-- Carousel -->
+
         <div class="content">
           <!-- Quick Stats -->
           <!-- jQuery Sparkline (.js-sparkline class is initialized in Helpers.jqSparkline() -->
